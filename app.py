ac49feb4c2ce652015cbe2aa968e594c66e8db6b
zebulon75018 <zebulon75018@yahoo.fr>
2019-05-18 23:35:58 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -7,13 +7,31 @@ import os
 from os.path import isfile, join
 
 app = Flask(__name__)
-
+listvideo = os.listdir("static/video")
+percent = 100/(len(listvideo))
 app.debug = True
+
+
+
+@app.route('/wip')
+def wip():    
+    return render_template('indexwip.html',listvideo=listvideo, percent= percent)
+
+@app.route('/porto')
+def porto():    
+    return render_template('indexPorto.html',listvideo=listvideo, percent= percent)
+
+@app.route('/multi')
+def multi():    
+    return render_template('indexmulti.html',listvideo=listvideo, percent= percent)
+
 @app.route('/')
 def index():
-    listvideo = os.listdir("static/video")
-    percent = 100/(len(listvideo))
-    return render_template('indexPorto.html',listvideo=listvideo, percent= percent)
+    urltitle = [ { 'url':"/wip", 'title':"together "},
+    { 'url':"/porto", 'title':"portfolio "},
+    { 'url':"/multi", 'title':"multi "}
+    ]    
+    return render_template('index.html',listvideo=listvideo, urltitle=urltitle)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
